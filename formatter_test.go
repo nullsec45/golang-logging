f52560f8af40df1af219853e08ed326c7575c8c6
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-16 21:58:12 +0700

    feat : formatter.

---
+++ formatter_test.go
@@ -0,0 +1,15 @@
+package golang_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestFormatter(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	logger.Info("Hello Logging")
+	logger.Warn("Hello Logging")
+	logger.Error("Hello Logging")
+}
